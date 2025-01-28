9c77585c1cddad4d55d82513da82742b23834b2c
nikolagajovic <iamnikolagajovic@gmail.com>
2025-01-28 22:24:48 +0100

    Implemented functionality for "income" and "expense" buttons

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-document.addEventListener('DOMContentLoaded', function() {
-const openNewTransactionBtn = document.getElementById('openNewTransactionBtn');
-const newTransaction = document.getElementById('newTransaction');
-const closeBtn = document.getElementsByClassName('btn-transaction-close')[0];
+document.addEventListener('DOMContentLoaded', () => {
+  const openNewTransactionBtn = document.getElementById('openNewTransactionBtn');
+  const newTransaction = document.getElementById('newTransaction');
+  const closeBtn = document.querySelector('.btn-transaction-close');
 
-openNewTransactionBtn.onclick = function () {
+  openNewTransactionBtn.addEventListener('click', () => {
     newTransaction.style.display = 'block';
-};
+  });
 
-closeBtn.onclick = function() {
+  closeBtn.addEventListener('click', () => {
     newTransaction.style.display = 'none';
-  };
+  });
+
+  const transactionCategory = document.querySelector('.transaction-category');
+  const addIncome = document.querySelector('.transaction-income');
+  const addExpense = document.querySelector('.transaction-expense');
+
+  addIncome.addEventListener('click', () => {
+    transactionCategory.style.display = 'none';
+    addIncome.style.backgroundColor = 'var(--secondary-color)';
+    addExpense.style.backgroundColor = 'var(--background-color)';
+  });
+
+  addExpense.addEventListener('click', () => {
+    transactionCategory.style.display = 'flex';
+    addExpense.style.backgroundColor = 'var(--secondary-color)';
+    addIncome.style.backgroundColor = 'var(--background-color)';
+  });
 
 });
+
